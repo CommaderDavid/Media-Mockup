1fecaa6d55b9d6e5b71bd1d3c9aa74aeb239330d
David Boedigheimer <stealth259@gmail.com>
2021-10-04 15:51:27 -0700

    Initial creation of media mockup.

---
+++ src/componets/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* Need to create a main and secondary header */}
+    </React.Fragment>
+  );
+}
+
+export default App;
