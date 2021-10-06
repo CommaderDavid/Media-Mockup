131a648bed267b9c40c8478e183ecb7f6c71e489
David Boedigheimer <stealth259@gmail.com>
2021-10-06 15:58:36 -0700

    Add all components to main app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-// import Header from "./Header";
+import FriendList from "./FriendList";
+import HeaderMain from "./HeaderMain";
 import PostList from "./PostList";
+import Self from "./Self";
+import SelfPost from "./SelfPost";
+import TopBar from "./TopBar";
 
 function App() {
   return (
     <React.Fragment>
+      <TopBar />
+      <HeaderMain />
       <PostList />
+      <FriendList />
+      <Self />
+      <SelfPost />
     </React.Fragment>
   );
 }
